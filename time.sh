add8883d188bbc397af797dcacd3bd8184124c1d
Alexander Libot <libot@mail.com>
2021-04-08 17:06:17 +0200

    Improved benchmark script and added second benchmark for rmdir

--- time.sh
+++ time.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-time (
-    for _ in {1..1000}; do
-        ./reset_disk.sh &>> /dev/null
-        ./target/release/rufs vdisk < benchmark &>> /dev/null
-    done
-)
+# Benchmark 1
+for prg in $(ls bin); do
+    echo "Benchmark 1: $prg"
+    time (
+        for _ in {1..10}; do
+            ./reset_disk.sh &>> /dev/null
+            bin/$prg vdisk < benchmarks/bm1 &>> /dev/null
+        done
+    )
+    echo
+done
+
+# Benchmark 2
+for prg in $(ls bin); do
+    echo "Benchmark 2: $prg"
+    time (
+        for _ in {1..1000}; do
+            ./reset_disk.sh &>> /dev/null
+            bin/$prg vdisk < benchmarks/bm2 &>> /dev/null
+        done
+    )
+    echo
+done

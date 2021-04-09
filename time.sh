0d87f6ab6700f15efaf4e84125b978601e98606f
Alexander Libot <libot@mail.com>
2021-04-09 14:08:19 +0200

    Added bm3 to remove dirs in reverse order and bm4 to add dirs with long names

--- time.sh
+++ time.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
-# Benchmark 1
-for prg in $(ls bin); do
-    echo "Benchmark 1: $prg"
-    time (
-        for _ in {1..10}; do
-            ./reset_disk.sh &>> /dev/null
-            bin/$prg vdisk < benchmarks/bm1 &>> /dev/null
-        done
-    )
-    echo
-done
-
-# Benchmark 2
-for prg in $(ls bin); do
-    echo "Benchmark 2: $prg"
-    time (
-        for _ in {1..1000}; do
-            ./reset_disk.sh &>> /dev/null
-            bin/$prg vdisk < benchmarks/bm2 &>> /dev/null
-        done
-    )
-    echo
+for bm in $(ls benchmarks); do
+    for prg in $(ls bin); do
+        echo "Benchmark $bm: $prg"
+        time (
+            for _ in {1..1000}; do
+                ./reset_disk.sh &>> /dev/null
+                bin/$prg vdisk < benchmarks/$bm &>> /dev/null
+            done
+        )
+        echo
+    done
 done
